f030e0b12e0f4a2a8048689df5bc6799f1030b62
AP0me <anar.abdullazada@ufaz.az>
2025-10-08 17:57:42 +0400

    batman

---
+++ main.py
@@ -0,0 +1,16 @@
+from mazelib import Maze
+from mazelib.generate.Prims import Prims
+import matplotlib.pyplot as plt
+
+def showPNG(grid):
+    """Generate a simple image of the maze."""
+    plt.figure(figsize=(10, 5))
+    plt.imshow(grid, cmap=plt.cm.binary, interpolation='nearest')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+m = Maze()
+m.generator = Prims(27, 34)
+m.generate()
+
+print(m)
